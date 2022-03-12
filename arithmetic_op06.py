169c902ec68701709a274aa8399cf38d1799a518
unknown <princariens@gmail.com>
2022-03-12 17:57:31 +0500

    uzgarish

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' with data type int.
 number=2987
 #Divide the number by 2 and assign number.
-number=number//2
+number=number/2
 #Find the fraction of the number and assing to a variable called 'answer'.
 answer=number%2
 #Print the answer.
